08d183ad8947b6801f738cf41d098f748c81e4b4
ArthurBombazaro <107115813+ArthurBombazaro@users.noreply.github.com>
2022-07-13 09:02:36 -0300

    Update exercicio2_pag33.java

--- exercicio2_pag33.java
+++ exercicio2_pag33.java
@@ -1,12 +1,18 @@
 public class exercicio2_pag33 {
     public static void main(String[] args) {
         System.out.println("Esses são os números primos entre 1 e 123: ");
-        for (i=2; i<123; i++){
-            for (teste=2;teste<i;teste++){
-                if (i % teste == 1){
-                    
-                } 
-            }
-        } 
+        for (int i=2; i<=123; i++){
+            if (ehprimo(i))
+            System.out.println(i);
+            } 
+        }   
+
+    private static Boolean ehprimo(int numero){
+        for (int j = 2; j < numero; j++){
+        if (numero % j == 0)
+        return false;
     }
-}
+        return true;        
+    }
+       
+} 
