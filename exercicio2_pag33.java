2abed062e527201533189dcb0b3f9aee65680cd6
ArthurBombazaro <107115813+ArthurBombazaro@users.noreply.github.com>
2022-07-13 08:31:04 -0300

    Add files via upload

---
+++ exercicio2_pag33.java
@@ -0,0 +1,12 @@
+public class exercicio2_pag33 {
+    public static void main(String[] args) {
+        System.out.println("Esses são os números primos entre 1 e 123: ");
+        for (i=2; i<123; i++){
+            for (teste=2;teste<i;teste++){
+                if (i % teste == 1){
+                    
+                } 
+            }
+        } 
+    }
+}
